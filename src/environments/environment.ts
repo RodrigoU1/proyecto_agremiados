5d5fed920efd4c65ce62486f729125fa48bf1755
RodrigoU1 <lolrodrigouriel@gmail.com>
2023-11-13 01:03:55 -0600

    Conexion con Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'agremiados-app',
+    appId: '1:50557816272:web:f200e5d6ab1aa38d607dd5',
+    storageBucket: 'agremiados-app.appspot.com',
+    apiKey: 'AIzaSyBUzPbmZoJkrXtQSE1JeaSgP3Q12r6s8CU',
+    authDomain: 'agremiados-app.firebaseapp.com',
+    messagingSenderId: '50557816272',
+  },};
